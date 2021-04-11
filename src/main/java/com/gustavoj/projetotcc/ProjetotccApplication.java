ba640ff043120377dbbe51b5c40f3a7349c98ed3
Gustavo Brito <gustavojordaob@outlook.com>
2021-04-11 13:47:28 -0300

    criação da entidade produto e associação de muitos para muitos com categoria

--- src/main/java/com/gustavoj/projetotcc/ProjetotccApplication.java
+++ src/main/java/com/gustavoj/projetotcc/ProjetotccApplication.java
@@ -1,8 +1,6 @@
 package com.gustavoj.projetotcc;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -10,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.gustavoj.projetotcc.domain.Categoria;
+import com.gustavoj.projetotcc.domain.Produto;
 import com.gustavoj.projetotcc.repositories.CategoriaRepository;
+import com.gustavoj.projetotcc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class ProjetotccApplication implements CommandLineRunner {
 
 	@Autowired
-	private CategoriaRepository repo;
+	private CategoriaRepository categoriaRepositories;
+	
+	@Autowired
+	private ProdutoRepository produtoRepositories;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetotccApplication.class, args);
@@ -27,7 +30,22 @@ public class ProjetotccApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(1,"Informatica");
 		Categoria cat2 = new Categoria(2,"Comida");
 		
-		repo.saveAll(Arrays.asList(cat1,cat2));
+		Produto prod1 = new Produto(null, "Computador", 2000.00);
+		Produto prod2 = new Produto(null, "Impressora", 800.00);
+		Produto prod3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(prod1,prod2,prod3));
+		cat2.getProdutos().addAll(Arrays.asList(prod2));
+		
+		prod1.getCategorias().add(cat1);
+		prod2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		prod3.getCategorias().addAll(Arrays.asList(cat1));
+
+		
+
+		categoriaRepositories.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepositories.saveAll(Arrays.asList(prod1,prod2,prod3));
+
 	}
 	
 	
