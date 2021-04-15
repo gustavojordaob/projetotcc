22a08a762d524eec2cfaa3a77249a5474df3b321
Gustavo Brito <gustavojordaob@outlook.com>
2021-04-14 21:45:43 -0300

    Criação das classes estado e cidade

--- src/main/java/com/gustavoj/projetotcc/ProjetotccApplication.java
+++ src/main/java/com/gustavoj/projetotcc/ProjetotccApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.gustavoj.projetotcc.domain.Categoria;
+import com.gustavoj.projetotcc.domain.Cidade;
+import com.gustavoj.projetotcc.domain.Estado;
 import com.gustavoj.projetotcc.domain.Produto;
 import com.gustavoj.projetotcc.repositories.CategoriaRepository;
+import com.gustavoj.projetotcc.repositories.CidadeRepository;
+import com.gustavoj.projetotcc.repositories.EstadoRepository;
 import com.gustavoj.projetotcc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class ProjetotccApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProdutoRepository produtoRepositories;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired 
+	private EstadoRepository estadoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetotccApplication.class, args);
@@ -41,8 +51,18 @@ public class ProjetotccApplication implements CommandLineRunner {
 		prod2.getCategorias().addAll(Arrays.asList(cat1,cat2));
 		prod3.getCategorias().addAll(Arrays.asList(cat1));
 
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
 		
+		Cidade c1 = new Cidade(null, "Uberlancia",est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
 
+		estadoRepository.saveAll(Arrays.asList(est1,est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 		categoriaRepositories.saveAll(Arrays.asList(cat1,cat2));
 		produtoRepositories.saveAll(Arrays.asList(prod1,prod2,prod3));
 
